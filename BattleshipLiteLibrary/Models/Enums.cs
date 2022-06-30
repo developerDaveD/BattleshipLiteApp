eff498b41fed74954c78f390829ae2e50cc37aa8
Dave Donatuccio <ddonatuccio@gmail.com>
2022-06-30 11:59:51 -0400

    Initial commit

---
+++ BattleshipLiteLibrary/Models/Enums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace BattleshipLiteLibrary.Models
+{
+    // 0 = placed, 1 = ship, 2 = miss, 3 = hit, 4 = sunk
+    public enum GridSpotStatus
+    {
+        Empty,
+        Ship,
+        Miss,
+        Hit,
+        Sunk
+    }
+
+}
+
